0e10d04b9810a5fc00d46a0d3a0d3d3990bfab57
Kay <kay.kazmierczak@outlook.com>
2024-03-04 14:04:05 +0000

    comments

--- modules/sg/main.tf
+++ modules/sg/main.tf
@@ -1,9 +1,11 @@
+#Allow HTTP security group
 resource "aws_security_group" "allow_http" {
   name        = "allow_http"
   description = "Allow http inbound and outbound traffic"
   vpc_id      = var.vpc_id
 }
 
+#Rule to allow incoming connection on port 3000
 resource "aws_security_group_rule" "allow_local" {
   type              = "ingress"
   from_port         = 3000
@@ -14,6 +16,7 @@ resource "aws_security_group_rule" "allow_local" {
   security_group_id = aws_security_group.allow_http.id
 }
 
+#Rule to allow incoming HTTP connections 
 resource "aws_security_group_rule" "allow_http_ingress" {
   type              = "ingress"
   from_port         = 80
@@ -24,6 +27,7 @@ resource "aws_security_group_rule" "allow_http_ingress" {
   security_group_id = aws_security_group.allow_http.id
 }
 
+#Rule to allow outgoing HTTP connections
 resource "aws_security_group_rule" "allow_http_egress" {
   type              = "egress"
   from_port         = 0
@@ -34,12 +38,14 @@ resource "aws_security_group_rule" "allow_http_egress" {
   security_group_id = aws_security_group.allow_http.id
 }
 
+#Allow HTTPS security group
 resource "aws_security_group" "allow_https" {
   name        = "allow_https"
   description = "Allow https inbound and outbound traffic"
   vpc_id      = var.vpc_id
 }
 
+#Rule to allow incoming HTTPS connections 
 resource "aws_security_group_rule" "allow_https_ingress" {
   type              = "ingress"
   from_port         = 443
@@ -50,6 +56,7 @@ resource "aws_security_group_rule" "allow_https_ingress" {
   security_group_id = aws_security_group.allow_https.id
 }
 
+#Rule to allow outgoing HTTPS connections 
 resource "aws_security_group_rule" "allow_https_egress" {
   type              = "egress"
   from_port         = 0
@@ -60,12 +67,14 @@ resource "aws_security_group_rule" "allow_https_egress" {
   security_group_id = aws_security_group.allow_https.id
 }
 
+#Allow SSH security group
 resource "aws_security_group" "allow_ssh" {
   name        = "allow_ssh"
   description = "Allow SSH inbound traffic from your IP"
   vpc_id      = var.vpc_id
 }
 
+#Rule to allow incoming SSH connection from one IP
 resource "aws_security_group_rule" "allow_ssh" {
   type              = "ingress"
   from_port         = 22
