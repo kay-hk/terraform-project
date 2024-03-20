92b69c3a5590b18c38bd7b6527c8f3a3f30e4b2c
Kay <kay.kazmierczak@outlook.com>
2024-03-20 13:28:56 +0000

    fixes, removal of commented out code

--- ami/main.tf
+++
@@ -1,8 +0,0 @@
-provider "aws" {
-  region = "eu-west-2"
-}
-
-resource "aws_ami_from_instance" "example" {
-  name               = "terraform-example"
-  source_instance_id = var.instance_id
-}
--- ami/variables.tf
+++
@@ -1,3 +0,0 @@
-variable "instance_id" {
-    type = string
-}
